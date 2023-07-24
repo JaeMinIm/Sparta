e5bb763169180a0b13398fc2558d3b02a265a1b3
JaeMinIm <45176409+JaeMinIm@users.noreply.github.com>
2023-07-24 17:54:04 +0900

    week2

---
+++ Week2/Calculator3_Interface.kt
@@ -0,0 +1,5 @@
+package com.example.st_1.week2
+
+interface Operation{
+    fun calculate(a : Double , b : Double ): Double
+}
---
+++ Week2/Calculator3_Mul.kt
@@ -0,0 +1,12 @@
+package com.example.st_1.week2
+
+class Calculator3_Mul : Operation {
+    override fun calculate(a: Double, b: Double): Double {
+        return a * b
+    }
+}
+
+
+
+//add(a: Int, b: Int): Double = (a + b).toDouble()
+

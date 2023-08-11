f7a79b2a03cc61d77314bfe244f5f2dc26edb3c3
JaeMinIm <45176409+JaeMinIm@users.noreply.github.com>
2023-08-11 09:40:06 +0900

    Add files via upload
    4~5주차 Level5 and 선발대 test

---
+++ Week4/Hardtest.kt
@@ -0,0 +1,11 @@
+package com.example.s_week4
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Hardtest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hardtest)
+    }
+}
--- Week4/Imformation.kt
+++ Week4/Imformation.kt
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import kotlin.random.Random
 
 class Imformation : AppCompatActivity() {
+
+    val randomimage = arrayOf(
+        R.drawable.main1,
+        R.drawable.image2,
+        R.drawable.image3,
+        R.drawable.image4,
+        R.drawable.image5,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.impormation)
+
+        randomImage()
 
         val intent = intent
         val input_id = intent.getStringExtra("login_id")
@@ -21,11 +34,12 @@ class Imformation : AppCompatActivity() {
         end_bt.setOnClickListener{
             finish()
         }
+    }
 
-
-
-
-
-
+    private fun randomImage() {
+        val impor_image : ImageView = findViewById(R.id.impor_image)
+        val randomindex = Random.nextInt(randomimage.size)
+        val randomimageresource = randomimage[randomindex]
+        impor_image.setImageResource(randomimageresource)
     }
 }
